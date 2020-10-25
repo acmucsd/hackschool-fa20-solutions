ae685c1f86d70165825e56038f375d682d16b062
chanhenry54 <hechan@ucsd.edu>
2020-10-24 23:18:27 -0700

    Part 3 solution code

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+import CreatePokemon from './pages/CreatePokemon';
+import ViewPokemon from './pages/ViewPokemon';
+
+const App = () => {
+  return (
+    <div className="App">
+      <CreatePokemon />
+    </div>
+  );
+}
+
+export default App;
