dc25eacc802b4e09354b5ec760955482dc782079
Shravan Hariharan <shravanhariharan2@gmail.com>
2020-11-16 11:27:35 -0800

    remove extra route

--- server/api/index.js
+++ server/api/index.js
@@ -9,11 +9,6 @@ router.get('/pokemon', async (req, res) => {
   res.status(200).json({ pokemon });
 })
 
-router.get('/pokemon/:id', async (req, res) => {
-  const pokemon = await Pokemon.findById(req.params.id).exec();
-  res.status(200).json({ pokemon });
-})
-
 router.post('/pokemon', async (req, res) => {
   const { pokemon } = req.body;
   const newPokemon = await Pokemon.create(pokemon);
